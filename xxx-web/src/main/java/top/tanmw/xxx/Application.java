a0044c49521680b50a8c92623c44a4b5859dc783
xiaYuTian11 <38251546+xiaYuTian11@users.noreply.github.com>
2022-01-26 16:38:20 +0800

    添加初始化程序

---
+++ xxx-web/src/main/java/top/tanmw/xxx/Application.java
@@ -0,0 +1,17 @@
+package top.tanmw.xxx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author TMW
+ * @since 2022/1/26 16:36
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
